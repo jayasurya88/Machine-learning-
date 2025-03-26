82c82a363d6d77f1d8a469fd796b3cd659f45ac3
jayasurya88 <jayasurya7879@gmail.com>
2025-03-26 12:14:10 +0530

    updated allowed host in setiings.py

--- machine_learning/settings.py
+++ machine_learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_r*kkcnl-xec@%$uv+)6++=u#4ua4+#!$u_0vo0tnh*mn8jw!0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['machine-learning-i5e5.onrender.com', 'localhost','127.0.0.1']
 
 
 # Application definition
